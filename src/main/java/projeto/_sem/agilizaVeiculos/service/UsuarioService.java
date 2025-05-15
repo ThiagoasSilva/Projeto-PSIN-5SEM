92fa01936dd10d224963c9d9ac9841d547ff52be
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-05-14 23:14:03 -0300

    implementação de usuario service

---
+++ src/main/java/projeto/_sem/agilizaVeiculos/service/UsuarioService.java
@@ -0,0 +1,39 @@
+package projeto._sem.agilizaVeiculos.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import projeto._sem.agilizaVeiculos.entity.Usuario.Usuario;
+import projeto._sem.agilizaVeiculos.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+    private UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    public List<Usuario> criarUsuario(Usuario usuario) {
+        usuarioRepository.save(usuario);
+        return listarUsuario();
+    }
+
+    public List<Usuario> listarUsuario() {
+        Sort sort = Sort.by("idUsuario").ascending();
+        return usuarioRepository.findAll(sort);
+    }
+
+    public List<Usuario> modificarUsuario() {
+        usuarioRepository.save(usuario);
+        return listarUsuario();
+    }
+
+    public List<Usuario> deletarUsuario(int idUsuario) {
+        usuarioRepository.deleteById(idUsuario);
+        return listarUsuario();
+    }
+
+}
