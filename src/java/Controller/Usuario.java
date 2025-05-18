0d86b6451b64da4e4e4aeaff0f3594ac24af3f6f
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-05-18 16:59:33 -0300

    Contrução de projeto com JSP e Servlet

---
+++ src/java/Controller/Usuario.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller;
+
+import Enuns.Acesso;
+import java.time.LocalDate;
+
+/**
+ *
+ * @author thiagosilva
+ */
+public class Usuario {
+    private int idUsuario;
+    private Acesso acesso; //admin ou cliente
+    private String cpf;
+    private String rg;
+    private String nome; // na database recebe nome completo
+    private int idade;
+    private LocalDate nascimento;
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Acesso getAcesso() {
+        return acesso;
+    }
+
+    public void setAcesso(Acesso acesso) {
+        this.acesso = acesso;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public LocalDate getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(LocalDate nascimento) {
+        this.nascimento = nascimento;
+    }
+    
+    
+    
+}
